41a820479d618b66bdae74e1e1636c24d5bf508c
Alquipo <alquiponeto@outlook.com.br>
2021-04-01 17:45:35 -0300

    fix: session

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn,useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
@@ -9,9 +9,9 @@ export function SignInButton() {
 
   console.log(session)
   return session ? (
-    <button className={styles.signInButton} type="button">
+    <button className={styles.signInButton} type="button" onClick={()=> signOut()}>
       <FaGithub color="#04d361"/>
-      Alquipo Neto
+        {session.user.name}
       <FiX color="737380" className={styles.closeIcon}/>
     </button>
   ) : (
