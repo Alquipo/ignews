331cabc4b494009b98a3e6727d5ab60e0d73035c
Alquipo <alquiponeto@outlook.com.br>
2021-04-02 18:29:42 -0300

    feat: activeLink

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -50,6 +50,7 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   })
 
+  console.log(response)
   const posts = response.results.map(post => {
     return {
       slug: post.uid,
