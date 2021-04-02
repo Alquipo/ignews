b33e6b2c678d049e013c80e1b2175dbf48bdb7b4
Alquipo <alquiponeto@outlook.com.br>
2021-04-02 13:49:51 -0300

    fix: webhooks stripe

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -2,7 +2,7 @@ import { query as q } from 'faunadb'
 import { fauna } from "../../../services/fauna";
 import { stripe } from '../../../services/stripe';
 
-export async function saveSubscriptions(subscriptionID: string, customerId: string) {
+export async function saveSubscriptions(subscriptionID: string, customerId: string, createAction = false) {
   const userRef = await fauna.query(
     q.Select(
       "ref",
@@ -24,10 +24,27 @@ export async function saveSubscriptions(subscriptionID: string, customerId: stri
     price_id: subscription.items.data[0].price.id
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData}
+  if( createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData}
+      )
     )
-  )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionID
+            )
+          )
+        ),
+        {data: subscriptionData}
+      )
+    )
+  }
 }
