54a920fb5a1ca09383ff2c55c70b72483c9a714e
Alquipo <alquiponeto@outlook.com.br>
2021-04-02 17:27:46 -0300

    fix: link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -8,8 +10,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="id.news"/>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
